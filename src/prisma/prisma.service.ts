3fb52a866db3d1877590a7016110fb1148c5930e
Jack Banning <jack1119banning@gmail.com>
2023-02-27 14:40:43 -0500

    database connected to AWS rds services and able to view through pgAdmin4

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          //   url: 'postgresql://postgres:123@localhost3000/nest?schema=public',
+          url: 'postgresql://postgres:password@discgolf-db.catbyilbmjzp.us-east-1.rds.amazonaws.com:5432/discgolf-db?schema=public',
+        },
+      },
+    });
+  }
+}
